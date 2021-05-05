15e758da2340bec9a6156036999895530fac76ac
kuczynskil <l.kuczynski95@gmail.com>
2021-05-05 12:40:58 +0200

    Initial commit

---
+++ src/main/java/com/empik/numberadder/NumberAdderApplication.java
@@ -0,0 +1,13 @@
+package com.empik.numberadder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberAdderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberAdderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/empik/numberadder/NumberAdderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empik.numberadder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberAdderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
