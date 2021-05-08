95275ee9340c819e52bce391a5093ed11d93516c
kuczynskil <l.kuczynski95@gmail.com>
2021-05-08 17:37:12 +0200

    Add Swagger configuration

--- src/main/java/com/empik/numberadder/NumberAdderApplication.java
+++ src/main/java/com/empik/numberadder/NumberAdderApplication.java
@@ -2,6 +2,14 @@ package com.empik.numberadder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class NumberAdderApplication {
@@ -10,4 +18,26 @@ public class NumberAdderApplication {
         SpringApplication.run(NumberAdderApplication.class, args);
     }
 
+    @Bean
+    public Docket swaggerConfiguration() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.empik"))
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Number-sum API",
+                "API of a microservice, which sums numbers in a String",
+                "1.0",
+                "Free to use",
+                new Contact("Łukasz Kuczyński", "https://github.com/kuczynskil",
+                        "l.kuczynski95@gmail.com"),
+                "API license",
+                "https://github.com/kuczynskil",
+                Collections.emptyList()
+        );
+    }
 }
