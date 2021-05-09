6d0f9be0f3ebc23bb9594114e9030a7b6062235d
kuczynskil <l.kuczynski95@gmail.com>
2021-05-09 16:07:42 +0200

    Add Spring Eureka configuration

--- src/main/java/com/empik/numberadder/NumberAdderApplication.java
+++ src/main/java/com/empik/numberadder/NumberAdderApplication.java
@@ -2,6 +2,7 @@ package com.empik.numberadder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -12,6 +13,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import java.util.Collections;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class NumberAdderApplication {
 
     public static void main(String[] args) {
